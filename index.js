c63fe5b3abf92a22da1aa482d5c9ad371f5a91ce
MarcoAOliari <marcoaoliari@gmail.com>
2021-09-05 17:20:35 -0300

    authentication

--- index.js
+++ index.js
@@ -8,52 +8,59 @@ app.get("/", async (req, res) => {
 
     console.log("Autenticando...")
 
-    // Auth object from Google API
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-    })
+    // Try to authenticate
+    try {
+        // Auth object from Google API
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "credentials.json",
+            scopes: "https://www.googleapis.com/auth/spreadsheets",
+        })
 
-    // Create client instance for auth
-    const client = await auth.getClient();
+        // Create client instance for auth
+        const client = await auth.getClient();
 
-    // Instance of Google Sheets API
-    const googleSheets = google.sheets({ version: "v4", auth: client })
+        // Instance of Google Sheets API
+        const googleSheets = google.sheets({ version: "v4", auth: client })
 
-    const spreadsheetId = "1otlNIB4LfYPWLuKNmXN4f45PKewDZLsqsSOnHZg_icA"
+        const spreadsheetId = "1otlNIB4LfYPWLuKNmXN4f45PKewDZLsqsSOnHZg_icA"
 
-    console.log("Autenticação realizada com sucesso!")
-    console.log("Lendo a planilha...")
+        console.log("Autenticação realizada com sucesso!")
+        console.log("Lendo a planilha...")
 
-    // Read rows from spreadsheet
-    const getRows = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId,
-        range: "engenharia_de_software!A4:F"
-    })
+        // Read rows from spreadsheet
+        const getRows = await googleSheets.spreadsheets.values.get({
+            auth,
+            spreadsheetId,
+            range: "engenharia_de_software!A4:F"
+        })
 
-    console.log("Leitura completa com sucesso!")
-    console.log("Processando informações...")
+        console.log("Leitura completa com sucesso!")
+        console.log("Processando informações...")
 
-    // Evaluate all students from the spreadsheet
-    let data = processGrades(60, getRows.data.values)
+        // Evaluate all students from the spreadsheet
+        let data = processGrades(60, getRows.data.values)
 
-    console.log("Informações processadas com sucesso!...")
-    console.log("Escrevendo na planilha...")
+        console.log("Informações processadas com sucesso!...")
+        console.log("Escrevendo na planilha...")
 
-    // Write data into the spreadsheet
-    await googleSheets.spreadsheets.values.update({
-        auth,
-        spreadsheetId,
-        range: `engenharia_de_software!G4:H${data.length + 3}`,
-        valueInputOption: "USER_ENTERED",
-        resource: {
-            values: data
-        }
-    })
+        // Write data into the spreadsheet
+        await googleSheets.spreadsheets.values.update({
+            auth,
+            spreadsheetId,
+            range: `engenharia_de_software!G4:H${data.length + 3}`,
+            valueInputOption: "USER_ENTERED",
+            resource: {
+                values: data
+            }
+        })
 
-    console.log("Planilha preenchida com sucesso!")
-    res.sendStatus(200)
+        console.log("Planilha preenchida com sucesso!")
+        res.sendStatus(200)
+
+    } catch (e) {
+        console.log("Insira o arquivo credentials.json com as credenciais do Google Cloud para realizar a autenticação!")
+        res.sendStatus(403)
+    }
 })
 
 // App listens to 1337 port
